e53d9a2a965c2fb4a3f953bd1a6779c717f7b575
gunjan <dhiman.gunjan07@gmail.com>
2026-08-26 22:35:13 +0530

    list the products ordered in a period

---
+++ easy/LC1327.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select p.product_name as product_name, sum(o.unit) as unit from Products p
+join Orders o
+on p.product_id=o.product_id
+where year(o.order_date)='2020' and MONTH(o.order_date)='02'
+group by p.product_id
+having sum(o.unit)>=100;
