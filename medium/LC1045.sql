212cd2d2cf8b1d9c11b2a17f65c38d8607bf6e48
Gunjan Dhiman <dhiman.gunjan07@gmail.com>
2026-09-01 21:42:03 +0530

    Customer who bought all the changes

--- medium/LC1045.sql
+++ medium/LC1045.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select customer_id from customer 
 group by customer_id
-Having count(DISTINCT product_key)= (select COUNT(product_key) from Product);
+Having count(DISTINCT product_key)= (select COUNT(product_key) from Product); 
