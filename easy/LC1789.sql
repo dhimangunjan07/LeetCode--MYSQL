d7dc190c912a7655759200d76660b2c4adc6ee44
Gunjan Dhiman <dhiman.gunjan07@gmail.com>
2026-09-01 21:44:27 +0530

    Primary department for each employee

--- easy/LC1789.sql
+++ easy/LC1789.sql
@@ -3,4 +3,4 @@ select distinct employee_id,department_id from Employee
 where primary_flag="Y" OR employee_id in (select employee_id
 from Employee      
 group by employee_id
-having count(employee_id)=1);
+having count(employee_id)=1); 
