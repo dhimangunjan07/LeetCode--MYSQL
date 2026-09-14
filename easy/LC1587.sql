53c4176cb4704cb4ab04ce3b9af3511ae6cf1acd
Gunjan Dhiman <dhiman.gunjan07@gmail.com>
2026-09-14 19:53:24 +0530

    BANK ACCOUNT SUMMARY 2

---
+++ easy/LC1587.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT u.name, SUM(amount) as balance
+FROM Transactions t
+LEFT JOIN Users u ON u.account=t.account GROUP BY t.account
+HAVING SUM(amount)>10000
